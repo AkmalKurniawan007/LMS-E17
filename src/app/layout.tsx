98c3cb81396b82a49b3bac211e9ec63c43c640eb
AkmalKurniawan007 <akmalikhsan97@gmail.com>
2026-09-21 14:20:28 +0700

    Suppress browser extension hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="id"
+      suppressHydrationWarning
       className={`${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans">
+      <body suppressHydrationWarning className="min-h-full flex flex-col font-sans">
         {children}
         <Toaster position="top-right" richColors />
       </body>
